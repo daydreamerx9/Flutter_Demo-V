96ff84dc224b78e891a7be4e3243e4aaa98b0ef4
xiangyuhang <905420946@qq.com>
2020-03-23 21:06:50 +0800

    text com

---
+++ lib/widget/demowidget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class demoWidget extends StatelessWidget {
+  const demoWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("data"),
+    );
+  }
+}
